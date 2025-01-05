2f116ebc5e8df4d09750d3ec55b3ee4fd284aea6
Lucas Pinheiro <pinheiro.lucash@gmail.com>
2025-01-05 11:31:42 -0300

    criando teste que faz 2 comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,26 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
+
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve cadastrar dois comentarios', () => {
+            const {debug} = render(<PostComment/>);
+            fireEvent.change(screen.getByTestId('add-comentario'), {
+                target: { value: 'Legal' }
+            })
+            fireEvent.click(screen.getByTestId('btn-cadastrar'))
+            expect(screen.getByText('Legal')).toBeInTheDocument()
+
+            fireEvent.change(screen.getByTestId('add-comentario'), {
+                target: { value: 'Bonitinho'}
+            })
+            fireEvent.click(screen.getByTestId('btn-cadastrar'))
+            expect(screen.getByText('Bonitinho')).toBeInTheDocument()
+
+        })
 });
